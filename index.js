35076a8f42bdfd779550112b4513e77d3235e955
OpTudor <133231902+OpTudor@users.noreply.github.com>
2023-06-04 20:09:43 +0300

    Add besic operation fucntions

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  console.log(a + b);
+}
+
+function subtract(a, b) {
+  console.log(a - b);
+}
+
+function multiply(a, b) {
+  console.log(a * b);
+}
+
+function divide(a, b) {
+  console.log(a / b);
+}
+
+let firstNumber, secondNumber, operation;
+
+function operate(firstNumber, secondNumber, operation) {
+  switch (operation) {
+    case "+":
+      add(firstNumber, secondNumber);
+      break;
+    case "-":
+      subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      divide(firstNumber, secondNumber);
+      break;
+  }
+}
