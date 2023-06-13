6ddd62c2521bf7062b1b3c85c4d1d08ff6f17d8d
OpTudor <133231902+OpTudor@users.noreply.github.com>
2023-06-13 19:02:55 +0300

    Fix bugs
    -you can use multiple digit numbers
    -you can use decimal points as intended
    -times 0 and divide by 0 returns NaN
    -broke the change sign function :)
    -max length of number added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const numberButton = document.querySelectorAll(".numberButton");
-const actionButton = document.querySelectorAll(".actionButton");
+const clearButton = document.querySelector(".clearButton");
 const operationButton = document.querySelectorAll(".operationButton");
 const display = document.getElementById("display");
 const equalButton = document.querySelector(".equalButton");
+const signButton = document.querySelector(".signButton");
+const decimalButton = document.querySelector(".decimalButton");
 let operated = false;
+display.textContent = 0;
 
 function add(a, b) {
   return Number(a) + Number(b);
@@ -46,38 +49,88 @@ function operate(firstNumber, secondNumber, operation) {
 numberButton.forEach((number) => {
   number.addEventListener("click", () => {
     changeDisplay(number.textContent);
-    if (
-      (result === 0 && operateSign === "") ||
-      firstNumber === 0 ||
-      operated === true
-    ) {
-      firstNumber = number.textContent;
-      operated = false;
-    } else secondNumber = number.textContent;
+    if (operateSign === "") {
+      firstNumber = Number(display.textContent);
+    } else {
+      secondNumber = Number(display.textContent);
+    }
   });
 });
+
 operationButton.forEach((operation) => {
   operation.addEventListener("click", () => {
     operateSign = operation.textContent;
     operated = false;
     result = 0;
+    display.textContent = "";
   });
 });
+
 function changeDisplay(number) {
-  display.textContent = number;
+  if (
+    (number === "." && display.textContent.includes(".")) ||
+    display.textContent.length > 7
+  ) {
+    return;
+  }
+
+  if (
+    (display.textContent === "0" && number !== ".") ||
+    display.textContent === "NaN"
+  ) {
+    display.textContent = number;
+  } else {
+    display.textContent += number;
+  }
 }
 
 equalButton.addEventListener("click", () => {
-  if (operateSign !== "" && secondNumber !== 0) {
+  if (secondNumber === 0 && (operateSign === "/" || operateSign === "*")) {
+    changeDisplay("NaN");
+    operated = true;
+    firstNumber = result;
+    secondNumber = 0;
+    operateSign = "";
+  } else if (operateSign !== "") {
     if (firstNumber === 0) {
       operate(result, secondNumber, operateSign);
     } else {
       operate(firstNumber, secondNumber, operateSign);
     }
+    display.textContent = "";
     changeDisplay(result);
     operated = true;
     firstNumber = result;
     secondNumber = 0;
     operateSign = "";
+  }
+});
+
+clearButton.addEventListener("click", () => {
+  display.textContent = 0;
+  firstNumber = 0;
+  secondNumber = 0;
+  operateSign = "";
+});
+
+signButton.addEventListener("click", () => {
+  if (display.textContent === firstNumber) {
+    firstNumber = 0 - firstNumber;
+    changeDisplay(firstNumber);
+  } else {
+    secondNumber = 0 - secondNumber;
+    changeDisplay(secondNumber);
+  }
+});
+
+decimalButton.addEventListener("click", () => {
+  if (operateSign === "" && !String(firstNumber).includes(".")) {
+    firstNumber = String(firstNumber) + ".";
+    display.textContent = "";
+    changeDisplay(firstNumber);
+  } else if (operateSign !== "" && !String(secondNumber).includes(".")) {
+    secondNumber = String(secondNumber) + ".";
+    display.textContent = "";
+    changeDisplay(secondNumber);
   }
 });
